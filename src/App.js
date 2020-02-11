6f9861f15478ec6131d96f76c2e889157c576904
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-02-11 16:38:37 +0100

    clean up app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      welcome
+    <div className='App'>
+      Type username:&nbsp;
+      <input
+        type="text"
+      /> <br />
+
+      Type password:&nbsp;
+      <input
+        type="text"
+      /> <br /><br />
+
+      Followers:
+      {
+        [
+          { id: 1, name: 'Luke' },
+          { id: 2, name: 'Petar' },
+        ].map(f => <div key={f.id}>{f.name}</div>)
+      } <br />
     </div>
   );
 }
